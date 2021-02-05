0fcd5de816d49d04ba019b80560486f820b6f715
pirousse <pierre.roussel.mailpro@gmail.com>
2021-02-05 11:20:27 +0100

    first commit

---
+++ build/precache-manifest.eddade57568a5da1c29fe60856959df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a89a2d5fb57feac1c0813eeb98d632",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "531e95ea3c2342e709b6",
+    "url": "/SnapShot/static/css/main.c6c3791b.chunk.css"
+  },
+  {
+    "revision": "0376548c8156e97414c9",
+    "url": "/SnapShot/static/js/2.5f102b53.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/SnapShot/static/js/2.5f102b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531e95ea3c2342e709b6",
+    "url": "/SnapShot/static/js/main.c06e1290.chunk.js"
+  },
+  {
+    "revision": "167331ad38a20a1217ed",
+    "url": "/SnapShot/static/js/runtime-main.54f9bf49.js"
+  }
+]);
