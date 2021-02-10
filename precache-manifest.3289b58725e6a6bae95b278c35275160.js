bd31b92d4d8aef3d37b8fe3294e8453dc754fb12
pirousse <pierre.roussel.mailpro@gmail.com>
2021-02-10 18:15:20 +0100

    Updates

---
+++ precache-manifest.3289b58725e6a6bae95b278c35275160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4e57faa25f2d272dc506d11a39b016",
+    "url": "/siteweb/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/siteweb/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "26178fe9a219d7a7a2e4",
+    "url": "/siteweb/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/siteweb/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "26178fe9a219d7a7a2e4",
+    "url": "/siteweb/static/js/main.dae77855.chunk.js"
+  },
+  {
+    "revision": "4cdcf1c93523a18bb244",
+    "url": "/siteweb/static/js/runtime~main.fe259394.js"
+  }
+]);
